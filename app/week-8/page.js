3330fc5bbbf714cf268238e6a9b9c488551cb5cb
Manraj Singh Rangi <singhrangimanraj@gmail.com>
2025-07-18 23:25:59 -0600

    week-5,6,7,8,9
    this is the code of all 5 weeks  ,

---
+++ app/week-8/page.js
@@ -0,0 +1,44 @@
+"use client";
+
+import { useState } from "react";
+import ItemList from "./item-list";
+import NewItem from "./new-item";
+import itemsData from "./items.json";
+import MealIdeas from "./meal-ideas";
+
+export default function Page() {
+  const [items, setItems] = useState(itemsData);
+  const [selectedItemName, setSelectedItemName] = useState("");
+
+  const handleAddItem = (item) => {
+    setItems([...items, { ...item, id: Date.now().toString() }]);
+  };
+
+  const handleItemSelect = (item) => {
+    const cleanedName = item.name
+      .split(",")[0]
+      .replace(
+        /([\u2700-\u27BF]|[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g,
+        ""
+      )
+      .trim();
+    setSelectedItemName(cleanedName);
+  };
+
+  return (
+    <main className="bg-pink-200 min-h-screen p-4">
+      <h1 className="font-bold font-serif text-3xl text-center mb-4 text-black">
+        Shopping List
+      </h1>
+      <div className="flex">
+        <div className="flex-1">
+          <NewItem onAddItem={handleAddItem} />
+          <ItemList items={items} onItemSelect={handleItemSelect} />
+        </div>
+        <div className="flex-1">
+          <MealIdeas ingredient={selectedItemName} />
+        </div>
+      </div>
+    </main>
+  );
+}
